cc85fca84edc895632bacb181421aee68f4beb02
1075803118 <1075803118@qq.com>
2018-12-10 12:15:12 +0800

    x

--- src/views/classificat/index.vue
+++ src/views/classificat/index.vue
@@ -7,6 +7,7 @@
                           ref="tabNav"
                           :data="TabTitle">
             </cube-tab-bar>
+
             <div class="tab-slide-container">
                 <cube-slide
                         ref="slide"
@@ -15,90 +16,24 @@
                         :show-dots=false
                         :initial-index="initialIndex"
                         :options="slideOptions"
-                        @change="changePage">
-                    <cube-slide-item v-for="(item,index) in TabTitle" v-if="TabTitle.length!==0" :key="index">
-                        <cube-scroll :options="scrollOptions">
-                            {{item.label}}
-                        </cube-scroll>
+                        :data="TabTitle"
+                        :refreshResetCurrent="true"
+                        @change="changePage"
+                        >
+                    <cube-slide-item v-for="(item,index) in TabTitle" :key="index" >
+                        <ul class="class-d-name" @touchstart="state" @touchmove="touchmoves(index,$event)">
+                            <li v-for="item in item.classDName">
+                                <img :src="`https://cdn.comicool.cn/${item.content_poster}?imageView2/2/w/180`">
+                                <div class="name">
+                                    <h3>{{item.content_title}}</h3>
+                                    <div class="name-icon">
+                                        <i class="cubeic-good"></i>
+                                        <p>{{item.content_praise_count}}</p>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
                     </cube-slide-item>
-
-
-
-                    <!--<cube-slide-item>-->
-                    <!--<cube-scroll  :options="scrollOptions">-->
-                    <!--123-->
-                    <!--</cube-scroll>-->
-                    <!--</cube-slide-item>-->
-                    <!--<cube-slide-item>-->
-                    <!--<cube-scroll  :options="scrollOptions">-->
-                    <!--123-->
-                    <!--</cube-scroll>-->
-                    <!--</cube-slide-item>-->
-                    <!--<cube-slide-item>-->
-                    <!--<cube-scroll  :options="scrollOptions">-->
-                    <!--123-->
-                    <!--</cube-scroll>-->
-                    <!--</cube-slide-item>-->
-                    <!--<cube-slide-item>-->
-                    <!--<cube-scroll  :options="scrollOptions">-->
-                    <!--123-->
-                    <!--</cube-scroll>-->
-                    <!--</cube-slide-item>-->
-                    <!--<cube-slide-item>-->
-                    <!--<cube-scroll  :options="scrollOptions">-->
-                    <!--123-->
-                    <!--</cube-scroll>-->
-                    <!--</cube-slide-item>-->
-                    <!--<cube-slide-item>-->
-                    <!--<cube-scroll  :options="scrollOptions">-->
-                    <!--123-->
-                    <!--</cube-scroll>-->
-                    <!--</cube-slide-item>-->
-                    <!--<cube-slide-item>-->
-                    <!--<cube-scroll  :options="scrollOptions">-->
-                    <!--123-->
-                    <!--</cube-scroll>-->
-                    <!--</cube-slide-item>-->
-                    <!--<cube-slide-item>-->
-                    <!--<cube-scroll  :options="scrollOptions">-->
-                    <!--123-->
-                    <!--</cube-scroll>-->
-                    <!--</cube-slide-item>-->
-                    <!--<cube-slide-item>-->
-                    <!--<cube-scroll  :options="scrollOptions">-->
-                    <!--123-->
-                    <!--</cube-scroll>-->
-                    <!--</cube-slide-item>-->
-                    <!--<cube-slide-item>-->
-                    <!--<cube-scroll  :options="scrollOptions">-->
-                    <!--123-->
-                    <!--</cube-scroll>-->
-                    <!--</cube-slide-item>-->
-                    <!--<cube-slide-item>-->
-                    <!--<cube-scroll  :options="scrollOptions">-->
-                    <!--123-->
-                    <!--</cube-scroll>-->
-                    <!--</cube-slide-item>-->
-                    <!--<cube-slide-item>-->
-                    <!--<cube-scroll  :options="scrollOptions">-->
-                    <!--123-->
-                    <!--</cube-scroll>-->
-                    <!--</cube-slide-item>-->
-                    <!--<cube-slide-item>-->
-                    <!--<cube-scroll  :options="scrollOptions">-->
-                    <!--123-->
-                    <!--</cube-scroll>-->
-                    <!--</cube-slide-item>-->
-                    <!--<cube-slide-item>-->
-                    <!--<cube-scroll  :options="scrollOptions">-->
-                    <!--123-->
-                    <!--</cube-scroll>-->
-                    <!--</cube-slide-item>-->
-                    <!--<cube-slide-item>-->
-                    <!--<cube-scroll  :options="scrollOptions">-->
-                    <!--123-->
-                    <!--</cube-scroll>-->
-                    <!--</cube-slide-item>-->
                 </cube-slide>
             </div>
         </div>
@@ -106,7 +41,7 @@
 </template>
 <script type="text/ecmascript-6">
     import twoHead from '@/components/twoHead/index.vue'
-    import {classAction} from '../../action/homeAction.js'
+    import {classAction,classContAction} from '../../action/homeAction.js'
     import {findIndexitem} from '../../assets/js/util'
     import {TAB} from '../../assets/js/tab'
     export default {
@@ -115,7 +50,7 @@
             return{
                 selectedLabel: '新作',
                 id:'',
-                TabTitle:TAB,
+                TabTitle:[],
                 name:'nicc',
                 width: document.body.clientWidth,
                 slideOptions: {
@@ -126,6 +61,8 @@
                 scrollOptions: {
                     directionLockThreshold: 0
                 },
+                statX:'',
+                flng:true
             }
         },
         computed: {
@@ -135,32 +72,89 @@
                 return index
             },
         },
-        created(){
-            this.$nextTick(e => {
-                // this.getTabTitle()
-            },20)
+        mounted(){
+            /* 默认加载头部，和第一个的数据 */
+            this.getTabTitle();
         },
         methods: {
-            changePage(current) {
+            state(e){
+                let clientX = e.targetTouches[0].clientX
+                this.statX = clientX
+
+                /* 不让请求无限触发 */
+                this.flng = true;
+            },
+            changePage(current){
                 this.selectedLabel = this.TabTitle[current].label
                 this.id = this.TabTitle[current].id
 
+                this.flng = true;
+                this._qqiu(current,this.TabTitle);
+            },
+            touchmoves(index,e) {
+                /* 传入当前的index进行 */
+                let [clientX,statX,TabTitle] = [e.targetTouches[0].clientX,this.statX,this.TabTitle];
+
+                let x = clientX - statX ;
+
+                /* 判断是左边还是右边 */
+                if(x>10){
+                    index--
+                }
+                if(x<10){
+                    index++
+                }
+
+                /* 限制index值 */
+                if(index<0){
+                    index = 0
+                }else if(index>TabTitle.length-1){
+                    index = TabTitle.length-1
+                }
+                
+                this._qqiu(index,TabTitle)
+            },
+            _qqiu(index,TabTitle){
+                if(this.flng){
+                    this.flng = false
+                    /* 判断已有的不在重复请求 */
+                    if(TabTitle[index].classDName.length ===0){
+                        this._clickClass(TabTitle[index].id)
+                    }
+                }
+            },
+            _clickClass(id){
+                /* 传入id 进行请求内容 */
+                let TabTitle = this.TabTitle
+                classContAction(id).then(res => {
+                    for(let i of TabTitle){
+                        if(i.id === id){
+                            i.classDName = res.content_list
+                        }
+                    }
+                });
+                this.TabTitle = TabTitle;
             },
             getTabTitle() {
+                let arr = []
                 classAction().then(res => {
-                    let arr = []
+
                     if(res.msg === "success"){
                         let category = res.category_list
                         for (let i in category) {
                             let msg = {
                                 label: category[i].category_title,
-                                id: category[i].category_id
+                                id: category[i].category_id,
+                                classDName:[]
                             }
                             arr.push(msg)
                         }
                     }
-                    this.TabTitle = arr
-                })
+                    /* 默认把第一个请求出来 */
+                    this._clickClass(arr[0].id)
+                });
+                this.TabTitle=arr
+
             }
         },
         components:{
@@ -170,6 +164,19 @@
 </script>
 
 <style lang="less" rel="stylesheet/less">
+
+    .flexWrap{
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+
+    .tab-slide-container{
+        width: 750px;
+    }
+    .cube-slide-item{
+       min-height: 1px;
+    }
     .tabox{
         position: absolute;
         z-index: 10;
@@ -186,6 +193,41 @@
                 width: 20%;
                 min-width: 20%;
                 max-width: 20%;
+            }
+        }
+    }
+
+
+    .class-d-name{
+        width: 750px;
+        .flexWrap;
+        li{
+            width: 33.3333%;
+            border:1px solid #ebebeb ;
+            border-width: 0 1px 1px 0;
+            box-sizing: border-box;
+            &:nth-child(3n){
+                border-right: none;
+            }
+            img{
+                width: 100%;
+                height: 150px;
+            }
+            .name{
+                position: relative;
+                padding: 26px 20px 15px;
+                height: 100px;
+                h3{
+                    font-size: 30px;
+                }
+                &-icon{
+                    .flexWrap;
+                    position: absolute;
+                    bottom: 15px;
+                    font-size: 24px;
+                    font-size: 25px;
+                    color: #00dd35;
+                }
             }
         }
     }
